166fd00c60a65053e47aa8f14dd68673e9c6b247
Piotr Obrebski <piotr.obrebski94@gmail.com>
2022-03-03 18:37:06 +0100

    Clarify instructions.

--- src/components/calculators/calculator-graph/information-section.tsx
+++ src/components/calculators/calculator-graph/information-section.tsx
@@ -6,8 +6,8 @@ export const InformationSection = () => {
       <ul>
         <li>To add player, hold shift and click on the grid.</li>
         <li>
-          To add edge with value, change the value on the input above, then hold
-          shift and click/drag to between players.
+          To add edge with value, change the value on the 'Edge value' input,
+          then hold shift and click/drag to between players.
         </li>
         <li>To delete a node or edge, click on it and press delete.</li>
         <li>Click and drag nodes to change their position.</li>
