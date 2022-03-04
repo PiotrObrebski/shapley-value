ab206d0087ced7ad3399e681ae2cd5acf1a8085e
Piotr Obrebski <piotr.obrebski94@gmail.com>
2022-03-04 14:24:17 +0100

    Updates

---
+++ precache-manifest.58ee77d3829d15ba049529c41aa3d0c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab0e95b8f90d461268d7f5b5247af5",
+    "url": "/shapley-value/index.html"
+  },
+  {
+    "revision": "24100c984acc671f9c7d",
+    "url": "/shapley-value/static/css/2.5d83b9bd.chunk.css"
+  },
+  {
+    "revision": "27463f6f668c7ac0f754",
+    "url": "/shapley-value/static/css/main.e40d4a31.chunk.css"
+  },
+  {
+    "revision": "24100c984acc671f9c7d",
+    "url": "/shapley-value/static/js/2.ff442e82.chunk.js"
+  },
+  {
+    "revision": "f9749187fa7b74c7d336902927265960",
+    "url": "/shapley-value/static/js/2.ff442e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27463f6f668c7ac0f754",
+    "url": "/shapley-value/static/js/main.8e9265f4.chunk.js"
+  },
+  {
+    "revision": "8d2dd9df25f0b8bfc7a1",
+    "url": "/shapley-value/static/js/runtime-main.4105d692.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shapley-value/precache-manifest.7d6234be398162d6e4eb2c7ddb7911cd.js"
+  "/shapley-value/precache-manifest.58ee77d3829d15ba049529c41aa3d0c8.js"
 );
 
 self.addEventListener('message', (event) => {
